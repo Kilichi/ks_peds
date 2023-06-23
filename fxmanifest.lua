f55e382f0c87fc2f6512b2f892887c0d07300983
Jordi <82180160+xxjordi@users.noreply.github.com>
2023-06-23 11:45:58 +0200

    Update fxmanifest.lua
    Removed an unnecessary thing and fix typo

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
-version '2.0.0'
+version '2.0.1'
 lua54 'yes'
+
+-- QBUS VERSION
 
 ui_page 'nui/index.html'
 files {
@@ -9,38 +11,18 @@ files {
 }
 
 client_scripts {
-
-    -- Config
     'shared/Config.lua',
-    
-    -- Client classes
     'client/classes/PedMenu.lua',
-    
-    -- Client main code
     'client/Main.lua'
-    
 }
 
 server_scripts {
-
-    -- Config
     'shared/Config.lua',
-
-    -- Mysql Async dependencie
     '@oxmysql/lib/MySQL.lua',
-    
-    -- Server classes
     'server/classes/PedManarger.lua',
-    'server/classes/Menu.lua',
-    
-    -- Server main code
     'server/Main.lua'
-
 }
 
 dependencies {
     '/server:4752'
 }
-
-escrow_ignore { 'shared/Config.lua' }
-dependency '/assetpacks'
